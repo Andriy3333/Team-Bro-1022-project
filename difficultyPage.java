d8988c0c22c6d8f1762bc02672727d4ca5347127
Harry <93809481+nate-diaz@users.noreply.github.com>
2022-03-22 16:05:37 -0400

    Add files via upload

---
+++ difficultyPage.java
@@ -0,0 +1,14 @@
+package com.example.a1022finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class difficultyPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_difficulty_page);
+    }
+}
