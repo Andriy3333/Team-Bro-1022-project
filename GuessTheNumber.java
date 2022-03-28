36b2b202ad2bde941e865efa728dde2026087f33
Andriy3333 <99999350+Andriy3333@users.noreply.github.com>
2022-03-28 16:06:40 -0400

    Add files via upload

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -1,43 +1,64 @@
+package com.example.project1022mac;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class GuessTheNumber {
+    private String username;
+    private String points;
     private int randomNumber;
     private int lives;
     private int guessed;
+    final static private int lowerLimit = 0;
+    private int upperLimit;
 
     public int getRandomNumber(int min, int max){
         return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 
-    public int getEasyNumber(){
+    public int getUpperLimit(){
+        return upperLimit;
+    }
+    public void startEasyDifficulty(){
         randomNumber = getRandomNumber(0, 10);
-        lives = 8;
+        lives = 10;
         guessed = 0;
-        return randomNumber;
+        upperLimit = 10;
     }
 
-    public int getMediumNumber(){
+    public void  startMediumDifficulty(){
         randomNumber = getRandomNumber(0,30);
         lives = 5;
         guessed = 0;
-        return randomNumber;
+        upperLimit = 30;
     }
 
-    public int getHardNumber(){
+    public void startHardDifficulty(){
         randomNumber = getRandomNumber(0, 100);
         lives = 3;
         guessed = 0;
+        upperLimit = 100;
+    }
+
+    public int getChosenNumber(){
         return randomNumber;
     }
 
     public boolean checkGuess(int guess){
-         if(guess == randomNumber){
-             guessed++;
-             return true;
-         }
-         else{
-             lives--;
-             return false;
-         }
+        if(guess == randomNumber){
+            guessed++;
+            return true;
+        }
+        else{
+            lives--;
+            return false;
+        }
+    }
+
+    public String higherOrLower(int guess){
+        if(getChosenNumber() > guess){
+            return  "Higher";
+        }
+        else{
+            return "Lower";
+        }
     }
 }
