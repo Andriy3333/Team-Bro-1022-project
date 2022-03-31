3419c07aceae796a97fe88a825ddebb33165dd87
Easenn <92197865+Easenn@users.noreply.github.com>
2022-03-31 15:21:13 -0400

    Add files via upload

---
+++ List.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import java.util.ArrayList;
+
+public class List {
+    private String username, score;
+    private ArrayList<GuessTheNumber> topLeaderboard;
+
+    public createLeaderboard(){
+        addAll;
+    }
+
+    public List(String username, String score) {
+        this.username = username;
+        this.score = score;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+}
